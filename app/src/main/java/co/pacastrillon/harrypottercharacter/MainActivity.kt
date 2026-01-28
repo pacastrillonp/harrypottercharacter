c440957d5d3cf87859c2fea450fa2e26d7f84431
Pablo Castrillón <pablo.castrillon@tekus.co>
2026-01-28 17:36:33 -0500

    first commit
    Init proyect

---
+++ app/src/main/java/co/pacastrillon/harrypottercharacter/MainActivity.kt
@@ -0,0 +1,47 @@
+package co.pacastrillon.harrypottercharacter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.pacastrillon.harrypottercharacter.ui.theme.HarryPotterCharacterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HarryPotterCharacterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HarryPotterCharacterTheme {
+        Greeting("Android")
+    }
+}
